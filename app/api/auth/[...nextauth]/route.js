d61f4728e4936544cfe0e226605376c24080d0ee
horlayinkahtechie <horlainkah2005@gmail.com>
2025-07-28 19:04:14 +0100

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,6 +7,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
     }),
   ],
   callbacks: {
